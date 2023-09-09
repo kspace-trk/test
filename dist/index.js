b3ad0078efb190c856b2a5979505ce9bf882ca27
kspace-trk <keigonakao.0825@gmail.com>
2023-09-09 14:53:01 +0900

    build

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,6 @@
-var e = (c, o) => () => (o || c((o = { exports: {} }).exports, o), o.exports);
-var l = e((t, n) => {
-  n.exports = {
-    function() {
-      console.log("----------------------");
-    }
-  };
-});
-export default l();
+function o() {
+  console.log("----------------------");
+}
+export {
+  o as default
+};
