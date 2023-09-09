af9c9809d6f61d595617335ac7783789179c4eab
kspace-trk <keigonakao.0825@gmail.com>
2023-09-09 14:55:36 +0900

    es build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'index',
-      fileName: 'index',
+      fileName: (format) => `index.${format}.js`,
     },
   },
 })
